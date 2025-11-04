282cfaeb39f379d4d1a8b2c976b4c9f1560a82ac
Mitesh Prajapati <mitesh5.mprajapati@gmail.com>
2025-11-04 12:08:24 +0530

    Add code to create user on signup

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,28 @@
-function add(req, res) {
+import { User } from "../models/user.model.js";
+
+async function add(req, res) {
+    // Get request data.
+    const { first_name, last_name, username, email, password } = req.body;
+
+    // Validate request data.
+    if (!first_name || !last_name || !username || !email || !password) {
+        res
+            .status(400)
+            .json({
+                status: 'Bad Request'
+            });
+
+        return;
+    }
+
     // Add user
-    console.log('req.body', req.body);
+    await User.create({
+        first_name: first_name,
+        last_name: last_name,
+        username: username,
+        email: email,
+        password: password
+    });
 
     res
         .status(201)
